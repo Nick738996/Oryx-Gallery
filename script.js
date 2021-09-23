ef972048eeedd432ba72cf7e186da7d01a74f71e
Nick738996 <Brandon7389@gmail.com>
2021-09-23 15:43:30 -0500

    Scroll-top Arrow
    Arrow up added with scroll top in CSS

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const hamburger = document.getElementById("hamburger");
 const headerMenu = document.getElementById("header-menu");
+const arrowUp = document.getElementById("arrow-up");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    arrowUp.style.display = "block";
+  } else {
+    arrowUp.style.display = "none";
+  }
+}
+
 
 hamburger.addEventListener("click", () =>
 {
-    console.log("tambien sirve");
+    
     headerMenu.classList.toggle("show");
 });
